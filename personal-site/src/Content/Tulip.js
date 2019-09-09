70c40e6d6a2029e9fef4e4e450d946bcbc0aae3f
ChristianCornelis <ccorneli@uoguelph.ca>
2019-09-09 13:50:44 -0400

    Fixing small spacing issues.

--- personal-site/src/Content/Tulip.js
+++ personal-site/src/Content/Tulip.js
@@ -12,7 +12,7 @@ but extremely capable, team is tasked with implementing new ideas into the Tulip
 developing demo environments for the sales team to use in pitches.\nThe first half of my term was spent working on the Demo Team (a subteam of the Innovation team), \
 where I quickly became knowledgeable in setting up a instances of the Tulip Platform, configuring specific details for sales pitch stores, and creating efficient ways \
 for sales engineers to become autonomous. This was a very interesting experience, as it gave me a very detailed look into Tulip’s entire stack, seeing as I had to configure \
-instances of the platform from scratch multiple times. Developing ways for sales engineers to become more independent was also rewarding, as it was a great opportunity to learn new technologies such as JavaScript, CSS, and jQuery.\n\
+instances of the platform from scratch multiple times. Developing ways for sales engineers to become more independent was also rewarding, as it was a great opportunity to learn new technologies such as JavaScript, CSS, and jQuery. \n\
 The second half of my term was spent working as a full-fledged member of the innovation team. I was given the chance to learn a lot about the Google Cloud Platform (GCP), \
 and was able to work on the development of a PHP library to handle creating and maintaining instances of Google Pub Sub message queues. Other notable projects included using \
 a machine learning GCP function to parse incoming texts and determine the emotion portrayed in them and working on a new, efficient way for customers to input their contact info \
@@ -47,13 +47,13 @@ export const goals = [
 ];
 
 export const conclusion = "To conclude, I feel that my work term at Tulip Retail was extremely successful. I gained a lot of development experience, which I was really hoping to achieve at the end of my previous two work terms, \
-and got to work as part of the amazing Innovation Team. My co-workers were all wonderful people to work with, and never hesitated to lend a helping hand whenever I was stuck with a problem or having issues.\n\
+and got to work as part of the amazing Innovation Team. My co-workers were all wonderful people to work with, and never hesitated to lend a helping hand whenever I was stuck with a problem or having issues. \n\
 After this work term, I feel that I am much more confident in my skills as a developer, and can approach difficult programming tasks with more knowledge and less hesitation. Both my technical and communication skills were heightened \
 dramatically during my time at Tulip Retail, and for that I am thankful.";
 
 export const acknowledgements = "I would like to thank Drew Matheson for all of his work as my designated buddy for the semester. His in-depth knowledge of much of the technology that I worked with over this work term was extremely \
 helpful whenever I turned to him with questions related to my work. I would also like to thank Amir Habibi for his constant feedback and accommodation as my manager over this work term. He’s an amazing leader and his guidance over the \
 semester was extremely helpful in advancing development skills and achieving my work term goals. Finally, I would like to the rest of my coworkers at Tulip Retail for providing an incredible work environment for this work term. Everyone’s \
-kindness and willingness to help whenever necessary is unprecedented in my experience.\n\
-I would also like to thank Laura Gatto and Kate McRoberts for their dedication to the co-op program and their constant support with any questions and concerns I have had, and continued to have, with matters related to co-op. Their support is extremely appreciated.\n\
+kindness and willingness to help whenever necessary is unprecedented in my experience. \n\
+I would also like to thank Laura Gatto and Kate McRoberts for their dedication to the co-op program and their constant support with any questions and concerns I have had, and continued to have, with matters related to co-op. Their support is extremely appreciated. \n\
 Lastly, I would like to thank Greg Klotz for his continued assistance throughout the years. A special thank you goes to Doctor Klotz for reviewing this report as well.";
